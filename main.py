3ccf3c750a04c09b3a5e95502e8e1d743fa8463c
lytwz <lytwzz@gmail.com>
2022-01-12 10:34:44 +0100

    message function

---
+++ main.py
@@ -0,0 +1,38 @@
+# bot.py
+import os
+
+import discord
+from dotenv import load_dotenv
+
+import random
+
+load_dotenv()
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    if message.content == '99!':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
+
+client.run()
